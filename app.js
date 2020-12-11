54ea009ca3a217f12b2aa518b0d484057ebd3870
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2020-12-11 16:52:22 -0700

    button cost and weapon quantity

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let clickUpgrades = {
         multiplier: 2
     },
     blast: {
-        price: 300,
+        price: 5,
         quantity: 0,
         multiplier: 3
     }
@@ -17,12 +17,12 @@ let clickUpgrades = {
 
 let automaticUpgrades = {
     drone: {
-        price: 1000,
+        price: 5,
         quantity: 0,
         multiplier: 5
     },
     squad: {
-        price: 5000,
+        price: 5,
         quantity: 0,
         multiplier: 5
     }
@@ -44,6 +44,43 @@ function buyLaser(){
         bl.quantity++
         bl.price=bl.price*bl.multiplier
         update()
-        console.log(bl.price, bl.quantity)
+        document.getElementById("laserCost").innerHTML = `Buy Laser $${bl.price}`
+        document.getElementById("laserCount").innerHTML = `Lasers = ${bl.quantity}`
+    }
+}
+
+function buyBlast(){
+    let bb = clickUpgrades.blast
+    if(dm >= bb.price){
+        dm = dm - bb.price
+        bb.quantity++
+        bb.price=bb.price*bb.multiplier
+        update()
+        document.getElementById("blastCost").innerHTML = `Buy Blast $${bb.price}`
+        document.getElementById("blastCount").innerHTML = `Blasters = ${bb.quantity}`
+    }
+}
+
+function buyDrone(){
+    let bd = automaticUpgrades.drone
+    if(dm >= bd.price){
+        dm = dm - bd.price
+        bd.quantity++
+        bd.price=bd.price*bd.multiplier
+        update()
+        document.getElementById("droneCost").innerHTML = `Buy Drone $${bd.price}`
+        document.getElementById("droneCount").innerHTML = `Drones = ${bd.quantity}`
+    }
+}
+
+function buySquad(){
+    let bs = automaticUpgrades.squad
+    if(dm >= bs.price){
+        dm = dm - bs.price
+        bs.quantity++
+        bs.price=bs.price*bs.multiplier
+        update()
+        document.getElementById("squadCost").innerHTML = `Buy Squad $${bs.price}`
+        document.getElementById("squadCount").innerHTML = `Squads = ${bs.quantity}`
     }
 }
